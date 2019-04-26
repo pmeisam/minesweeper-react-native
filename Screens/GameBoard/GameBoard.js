a3bc2cd575d49019c8666be52ac5d6439818276f
Meisam <pmeisam@ymail.com>
2019-04-26 16:48:01 -0700

    recursion function implemented

--- Screens/GameBoard/GameBoard.js
+++ Screens/GameBoard/GameBoard.js
@@ -32,8 +32,17 @@ class GameBoard extends Component {
     mines: []
   };
   handleArrayCheck = (x, y) => {
-    if (x < this.props.navigation.state.params.rows && y < this.props.navigation.state.params.rows && x >= 0 && y >= 0) {
-      if (this.state.mines.includes(y * this.props.navigation.state.params.rows + x)) {
+    if (
+      x < this.props.navigation.state.params.rows &&
+      y < this.props.navigation.state.params.rows &&
+      x >= 0 &&
+      y >= 0
+    ) {
+      if (
+        this.state.mines.includes(
+          y * this.props.navigation.state.params.rows + x
+        )
+      ) {
         return true;
       } else return false;
     } else return false;
@@ -42,7 +51,6 @@ class GameBoard extends Component {
     let counter = 0;
     const x = id % this.props.navigation.state.params.rows;
     const y = Math.floor(id / this.props.navigation.state.params.rows);
-    console.log(`(${x}, ${y})`);
     if (this.handleArrayCheck(x - 1, y - 1)) counter++;
     if (this.handleArrayCheck(x, y - 1)) counter++;
     if (this.handleArrayCheck(x + 1, y - 1)) counter++;
@@ -53,7 +61,96 @@ class GameBoard extends Component {
     if (this.handleArrayCheck(x + 1, y + 1)) counter++;
     return counter;
   };
+  handleReveal = (x, y) => {
+    const id = y * this.props.navigation.state.params.rows + x;
+    const rows = this.props.navigation.state.params.rows;
+    const columns = this.props.navigation.state.params.rows;
+    let board = this.state.board;
+    const count = this.handleFindMine(id);
+    const images = [
+      require("../../assets/images/0.png"),
+      require("../../assets/images/1.png"),
+      require("../../assets/images/2.png"),
+      require("../../assets/images/3.png"),
+      require("../../assets/images/4.png"),
+      require("../../assets/images/5.png"),
+      require("../../assets/images/6.png"),
+      require("../../assets/images/7.png"),
+      require("../../assets/images/8.png")
+    ];
+    if (x < 0 && y < 0 && x >= rows && y >= columns) return;
+    else if (this.handleFindMine(x, y) === 0) {
+      board[id] = require("../../assets/images/0.png");
+      if (this.handleFindMine(id - 1) === 0 && x > 0)
+        this.handleReveal(x - 1, y);
+      else if (this.handleFindMine(id - 1) !== 0 && x > 0) {
+        board[id - 1] = images[this.handleFindMine(id - 1)];
+      }
+      if (this.handleFindMine(id + 1) === 0 && x < rows - 1)
+        this.handleReveal(x + 1, y);
+      else if (this.handleFindMine(id + 1) !== 0 && x < rows - 1) {
+        board[id + 1] = images[this.handleFindMine(id + 1)];
+      }
+      if (this.handleFindMine(id + columns) === 0 && y < columns - 1)
+        this.handleReveal(x, y + 1);
+      else if (this.handleFindMine(id + columns) !== 0 && y < columns - 1) {
+        board[id + columns] = images[this.handleFindMine(id + columns)];
+      }
+      if (this.handleFindMine(id - columns) === 0 && y > 0)
+        this.handleReveal(x, y - 1);
+      else if (this.handleFindMine(id - columns) !== 0 && y > 0) {
+        board[id - columns] = images[this.handleFindMine(id - columns)];
+      }
+      if (this.handleFindMine(id - columns - 1) === 0 && x > 0 && y > 0)
+        this.handleReveal(x - 1, y - 1);
+      else if (this.handleFindMine(id - columns - 1) !== 0 && x > 0 && y > 0) {
+        board[id - columns - 1] = images[this.handleFindMine(id - columns - 1)];
+      }
+      if (
+        this.handleFindMine(id - columns + 1) === 0 &&
+        x < columns - 1 &&
+        y > 0
+      )
+        this.handleReveal(x + 1, y - 1);
+      else if (
+        this.handleFindMine(id - columns + 1) !== 0 &&
+        x < columns - 1 &&
+        y > 0
+      ) {
+        board[id - columns + 1] = images[this.handleFindMine(id - columns + 1)];
+      }
+      if (
+        this.handleFindMine(id + columns - 1) === 0 &&
+        x > 0 &&
+        y < columns - 1
+      )
+        this.handleReveal(x - 1, y + 1);
+      else if (
+        this.handleFindMine(id + columns - 1) !== 0 &&
+        x > 0 &&
+        y < columns - 1
+      ) {
+        board[id + columns - 1] = images[this.handleFindMine(id + columns - 1)];
+      }
+      if (
+        this.handleFindMine(id + columns + 1) === 0 &&
+        x < columns - 1 &&
+        y < columns - 1
+      )
+        this.handleReveal(x + 1, y + 1);
+      else if (
+        this.handleFindMine(id + columns + 1) !== 0 &&
+        x < columns - 1 &&
+        y < columns - 1
+      ) {
+        board[id + columns + 1] = images[this.handleFindMine(id + columns + 1)];
+      }
+      this.setState({ board });
+    }
+  };
   handlePressEmptyBox = id => {
+    const x = id % this.props.navigation.state.params.rows;
+    const y = Math.floor(id / this.props.navigation.state.params.rows);
     let board = this.state.board;
     if (this.state.mines.includes(id)) {
       board[id] = require("../../assets/images/mine.png");
@@ -71,7 +168,10 @@ class GameBoard extends Component {
         require("../../assets/images/7.png"),
         require("../../assets/images/8.png")
       ];
-      board[id] = images[count];
+      if (this.handleFindMine(id) === 0) this.handleReveal(x, y);
+      else {
+        board[id] = images[count];
+      }
       this.setState({ board });
     }
   };
